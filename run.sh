f89bbfcb3fd0f0c766b44a8c1d3c6b1a4f439c97
Erik <erik.eriksson@yahoo.com>
2016-03-29 23:39:26 +0200

    log rotate

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-nice ./ec3k-listen | grep is_on --line-buffered | tee >(./ha-feeder) >> ec3k.json
+nice ./ec3k-listen | grep is_on --line-buffered | tee >(./ha-feeder) | cronolog ec3k.%Y-%m-%d.ndjson
+
