5d3281bcb8634cca8e00e29b92abaf9b0f12e30f
Erik <erik.eriksson@yahoo.com>
2016-03-29 23:41:01 +0200

    log

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-nice ./ec3k-listen | grep is_on --line-buffered | tee >(./ha-feeder) | cronolog ec3k.%Y-%m-%d.ndjson
+nice ./ec3k-listen | grep is_on --line-buffered | tee >(./ha-feeder) | cronolog ec3k.%Y-%m-%d.log.ndjson
 
